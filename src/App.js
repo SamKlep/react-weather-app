071b7a8506bc4358d8cae1077a2157ac3cedef6d
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-02-23 00:23:14 -0600

    created styling for application

--- src/App.js
+++ src/App.js
@@ -24,39 +24,51 @@ class App extends React.Component {
         const data = await api_call.json();
         if (city && country) {
             // console.log(data);
-        this.setState({
-            temperature: data.list[0].main.temp,
-            city: data.city.name,
-            country: data.city.country,
-            humidity: data.list[0].main.humidity,
-            description: data.list[0].weather[0].main,
-            error: ""
-        });
+            this.setState({
+                temperature: data.list[0].main.temp,
+                city: data.city.name,
+                country: data.city.country,
+                humidity: data.list[0].main.humidity,
+                description: data.list[0].weather[0].main,
+                error: ""
+            });
         } else {
-        this.setState({
-            temperature: undefined,
-            city: undefined,
-            country: undefined,
-            humidity: undefined,
-            description: undefined,
-            error: "Please enter the values"
-        });
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter the values"
+            });
         }
 
     }
     render() {
         return (
             <div>
-                <Titles />
-                <Form getWeather={this.getWeather}/>
-                <Weather 
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                    />
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xs-5 title-container">
+                                    <Titles />
+                                </div>
+                                <div className="col-xs-7 form-container">
+                                    <Form getWeather={this.getWeather} />
+                                    <Weather
+                                        temperature={this.state.temperature}
+                                        city={this.state.city}
+                                        country={this.state.country}
+                                        humidity={this.state.humidity}
+                                        description={this.state.description}
+                                        error={this.state.error}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
